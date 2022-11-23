fd6f5bfad828d524d4682be29353d1a8c0afa06d
Pietro Fecarotta <108467431+Pietro-fe01@users.noreply.github.com>
2022-11-23 13:10:07 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import PageTitle from './components/PageTitle.vue';
+
+export default {
+  components: {
+    PageTitle
+  }
+}
+</script>
+
+<template>
+  <div>
+    <PageTitle/>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/PageTitle.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data() {
+    return {
+      title: 'Il titolo della pagina'
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style scoped></style>
+
+
